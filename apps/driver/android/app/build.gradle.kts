34788b3af522f444d1999b9fa0a2218301bb523e
iansilva23 <yansilva234@gmail.com>
2026-09-23 07:21:53 -0300

    build(android): align secure storage minSdk with v11

--- apps/driver/android/app/build.gradle.kts
+++ apps/driver/android/app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
 
     defaultConfig {
         applicationId = "br.com.ramonessa.driver"
-        minSdk = 23
+        minSdk = 24
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
