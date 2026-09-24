4223d87b61e2bce086a81c2c711d3eed310ab628
iansilva23 <yansilva234@gmail.com>
2026-09-24 13:21:14 -0300

    fix(ios): support Firebase Messaging with Swift Package Manager

--- apps/passenger/ios/Runner/Runner-Bridging-Header.h
+++ apps/passenger/ios/Runner/Runner-Bridging-Header.h
@@ -1,2 +1,7 @@
 #import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<firebase_messaging/FLTFirebaseMessagingPlugin.h>)
 #import <firebase_messaging/FLTFirebaseMessagingPlugin.h>
+#else
+@import firebase_messaging;
+#endif
