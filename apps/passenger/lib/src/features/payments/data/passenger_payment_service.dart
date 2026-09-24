c6fd520e379180a7c68fd54c5c55f0bb3759edcd
iansilva23 <yansilva234@gmail.com>
2026-09-24 00:37:31 -0300

    fix(passenger): unify cash payment service contract

--- apps/passenger/lib/src/features/payments/data/passenger_payment_service.dart
+++ apps/passenger/lib/src/features/payments/data/passenger_payment_service.dart
@@ -1,7 +1,5 @@
 import '../domain/cash_ride_authorization_result.dart';
 import '../domain/passenger_payment_policy.dart';
-import '../domain/cash_ride_payment_result.dart';
-import '../domain/payment_policy_snapshot.dart';
 import '../domain/wallet_ride_payment_result.dart';
 
 abstract interface class PassengerPaymentService {
@@ -11,6 +9,7 @@ abstract interface class PassengerPaymentService {
 
   Future<CashRideAuthorizationResult> authorizeCashRide({
     required String rideId,
+    required String idempotencyKey,
   });
 
   Future<WalletRidePaymentResult> payRideWithWallet({
