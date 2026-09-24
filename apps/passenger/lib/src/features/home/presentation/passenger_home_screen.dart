55aaf7667c21668e66d9b34c290c543131a922b4
iansilva23 <yansilva234@gmail.com>
2026-09-24 02:35:02 -0300

    fix(apps): import platform detection support

--- apps/passenger/lib/src/features/home/presentation/passenger_home_screen.dart
+++ apps/passenger/lib/src/features/home/presentation/passenger_home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:ramo_design_system/ramo_design_system.dart';
