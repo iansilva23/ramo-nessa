a1d9e093f956d4114ce9d71973d0c4349b8ad252
iansilva23 <yansilva234@gmail.com>
2026-09-24 16:56:56 -0300

    feat(passenger): configure OpenFreeMap Liberty style

--- apps/passenger/lib/src/core/config/ramo_map_config.dart
+++ apps/passenger/lib/src/core/config/ramo_map_config.dart
@@ -4,6 +4,9 @@ abstract final class RamoMapConfig {
   static const fallbackCenter = LatLng(-2.7956, -40.5142);
   static const fallbackZoom = 14.5;
 
+  static const openFreeMapStyleUrl =
+      'https://tiles.openfreemap.org/styles/liberty';
+
   static const osmTileUrl =
       'https://tile.openstreetmap.org/{z}/{x}/{y}.png';
 
