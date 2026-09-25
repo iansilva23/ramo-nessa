d31526c235dcda4f0d2a87a10e44a6b119674704
iansilva23 <yansilva234@gmail.com>
2026-09-25 00:18:29 -0300

    refactor(driver): remove legacy map provider config

--- apps/driver/lib/src/core/config/driver_map_config.dart
+++ apps/driver/lib/src/core/config/driver_map_config.dart
@@ -3,15 +3,4 @@ import 'package:latlong2/latlong.dart';
 abstract final class DriverMapConfig {
   static const fallbackCenter = LatLng(-2.7956, -40.5142);
   static const fallbackZoom = 14.5;
-
-  static const openFreeMapStyleUrl =
-      'https://tiles.openfreemap.org/styles/liberty';
-
-  static const osmTileUrl =
-      'https://tile.openstreetmap.org/{z}/{x}/{y}.png';
-  static const osrmBaseUrl = 'https://router.project-osrm.org';
-  static const requestTimeout = Duration(seconds: 10);
-
-  static const userAgent =
-      'RamoNessaDriver/0.1 (br.com.ramonessa.driver)';
 }
