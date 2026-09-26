57c2079556d66572411da118dbff4f6feb2c68ab
iansilva23 <yansilva234@gmail.com>
2026-09-26 02:52:53 -0300

    Remove Firebase Messaging bridge from passenger iOS

--- apps/passenger/ios/Runner/Runner-Bridging-Header.h
+++ apps/passenger/ios/Runner/Runner-Bridging-Header.h
@@ -1,7 +1,1 @@
 #import "GeneratedPluginRegistrant.h"
-
-#if __has_include(<firebase_messaging/FLTFirebaseMessagingPlugin.h>)
-#import <firebase_messaging/FLTFirebaseMessagingPlugin.h>
-#else
-@import firebase_messaging;
-#endif
