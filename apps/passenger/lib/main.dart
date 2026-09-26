a6b3b9a5f4be92b02f0507a78263085c17ea1ae7
iansilva23 <yansilva234@gmail.com>
2026-09-26 02:45:39 -0300

    Remove Firebase bootstrap from passenger

--- apps/passenger/lib/main.dart
+++ apps/passenger/lib/main.dart
@@ -1,25 +1,10 @@
 import 'dart:async';
 
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:ramo_design_system/ramo_design_system.dart';
 
-import 'firebase_options.dart';
 import 'src/app.dart';
 import 'src/core/auth/secure_auth_token_store.dart';
-import 'src/core/config/ramo_core_config.dart';
-import 'src/core/notifications/firebase_push_coordinator.dart';
-import 'src/core/notifications/http_push_device_service.dart';
-
-@pragma('vm:entry-point')
-Future<void> _firebaseMessagingBackgroundHandler(
-  RemoteMessage message,
-) async {
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-}
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -30,12 +15,10 @@ class _BootstrapData {
   const _BootstrapData({
     this.accessToken,
     this.clientInstanceId,
-    this.pushCoordinator,
   });
 
   final String? accessToken;
   final String? clientInstanceId;
-  final FirebasePushCoordinator? pushCoordinator;
 }
 
 class _PassengerBootstrap extends StatefulWidget {
@@ -58,28 +41,6 @@ class _PassengerBootstrapState extends State<_PassengerBootstrap> {
     final minimumSplash = Future<void>.delayed(
       const Duration(milliseconds: 1050),
     );
-
-    FirebasePushCoordinator? pushCoordinator;
-    try {
-      await Firebase.initializeApp(
-        options: DefaultFirebaseOptions.currentPlatform,
-      );
-      FirebaseMessaging.onBackgroundMessage(
-        _firebaseMessagingBackgroundHandler,
-      );
-
-      final coreUri = RamoCoreConfig.baseUri;
-      if (coreUri != null) {
-        pushCoordinator = FirebasePushCoordinator(
-          registry: HttpPushDeviceService(baseUrl: coreUri),
-        );
-        await pushCoordinator
-            .initialize()
-            .timeout(const Duration(seconds: 4));
-      }
-    } catch (_) {
-      // Firebase/push nunca pode bloquear a abertura do aplicativo.
-    }
 
     final tokenStore = SecureAuthTokenStore();
     String? accessToken;
@@ -104,7 +65,6 @@ class _PassengerBootstrapState extends State<_PassengerBootstrap> {
       _data = _BootstrapData(
         accessToken: accessToken,
         clientInstanceId: clientInstanceId,
-        pushCoordinator: pushCoordinator,
       );
     });
   }
@@ -125,7 +85,6 @@ class _PassengerBootstrapState extends State<_PassengerBootstrap> {
     return RamoNessaPassengerApp(
       accessToken: data.accessToken,
       clientInstanceId: data.clientInstanceId,
-      pushCoordinator: data.pushCoordinator,
     );
   }
 }
